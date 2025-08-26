11e374688d0bbccc1ad70d358235c1339ce9eb92
Mohd Asif <hiasif7786@gmail.com>
2025-08-26 22:12:22 +0530

    add cv

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -16,7 +16,7 @@ export const AboutSection = () => {
             </h3>
 
             <p className="text-muted-foreground">
-              With over 8 months of experience in web development, I specialize
+              With over one year of experience in web development, I specialize
               in creating responsive, accessible, and performant web
               applications using modern technologies.
             </p>
@@ -35,7 +35,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/projects/Mohd_Asif_resume1.pdf"
+                href="/projects/Mohd_Asif_resume.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300" download
               >
                 Download CV
